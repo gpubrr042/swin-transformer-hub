d508aee21bd9e8e66f8d2060e6ec3c2b0ffccca7
Vivek Talwar <24211231+real2vec@users.noreply.github.com>
2023-02-15 18:33:56 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -5,7 +5,7 @@ import swin
 
 MODEL_URLS = {
     "swin_tiny_patch4_window7_224": "https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_tiny_patch4_window7_224.pth"
-    "swin_base_patch4_window7_224": "https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_base_patch4_window7_224.pth"
+    "swin_base_patch4_window7_224": "https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_base_patch4_window7_224_22kto1k.pth"
 }
 
 def swin_tiny_patch4_window7_224(pretrained=False):
